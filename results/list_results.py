60b20a73711a29959d9ea7354fcd592bc5f628d5
Pulsar110 <125087940+Pulsar110@users.noreply.github.com>
2024-04-10 12:40:09 +0000

    fix script

--- results/list_results.py
+++ results/list_results.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
 plt.rcParams['xtick.labelsize'] = 8
 
+alpha_rate = 0.1
+
 legend_label_map = {
     'social_welfare_0': 'soc. wel. (Binary)',
     'social_welfare_1': 'soc. wel. (TypeCount)',
@@ -23,10 +25,9 @@ legend_label_map = {
 }
 
 ROOT = 'results/'
-# WORLDS = [CIRCLE_WORLD, CYLINDER_WORLD, GRID_4DEG_WORLD, GRID_8DEG_WORLD]
-WORLDS = ['CYLINDER_WORLD']
+WORLDS = ['CIRCLE_WORLD', 'CYLINDER_WORLD', 'GRID_4DEG_WORLD', 'GRID_8DEG_WORLD']
+# WORLDS = ['GRID_8DEG_WORLD']
 UTILITIES = ['BinaryDiversityUtility', 'TypeCountingDiversityUtility', 'DifferenceCountDiversityUtility', 'EntropyDivertiyUtility']
-#UTILITIES = [EntropyDivertiyUtility]
 INITIALIZATIONS = ['random_init', 'shelling_init'] #'block_init', 
 SWAP_CONDS = ['individual_greater', 'individual_no_worse', 'sum_greater']
 
@@ -38,12 +39,13 @@ for world in WORLDS:
     for initialization in INITIALIZATIONS:
         for swap_cond in SWAP_CONDS:
             print('Initialization: %s; Swap Condition: %s' % (initialization, swap_cond))
-            results = {'difference(final-init)': {}, 'final': {}, 'steps': {}}
-            dir_path = '%s/%s/%s' % (ROOT, initialization, swap_cond)
+            results = {'difference(final-init)': {}, 'final': {}, 'init':{}, 'steps': {}}
+            dir_path = '%s%s/%s' % (ROOT, initialization, swap_cond)
             for utility in UTILITIES:
                 short_label = utility.replace('DiversityUtility', '')
                 results['difference(final-init)'][short_label] = {}
                 results['final'][short_label] = {}
+                results['init'][short_label] = {}
                 with open('%s/%s_%s_results_.json' % (dir_path, world, utility), 'r') as jsonfile:
                     data = json.load(jsonfile)
                 results['steps'][short_label] = [d['steps'] for d in data.values()]
@@ -55,20 +57,20 @@ for world in WORLDS:
                         sub_data = np.array([d[label][k] for d in data.values()])
                         if label == 'init':
                             results['difference(final-init)'][short_label][k] = (results['final'][short_label][k] - sub_data)
-                        else:
-                            results[label][short_label][k] = sub_data
+                        results[label][short_label][k] = sub_data
                         mean_metrics = np.mean(sub_data, axis=0)
                         if isinstance(mean_metrics, np.ndarray):
                             mean_metrics = ','.join(list(map(str, mean_metrics)))
                         print('%s: %s' % (label, mean_metrics))
                 # add percentage_of_segregated_verticies
-                init = np.array([d['init']['degree_of_intergration'][0] for d in data.values()])
-                final = np.array([d['final']['degree_of_intergration'][0] for d in data.values()])
+                init = 1 - np.array([d['init']['degree_of_intergration'][0] for d in data.values()])
+                final = 1 - np.array([d['final']['degree_of_intergration'][0] for d in data.values()])
                 results['difference(final-init)'][short_label]['percentage_of_segregated_verticies'] = final - init
                 results['final'][short_label]['percentage_of_segregated_verticies'] = final
+                results['init'][short_label]['percentage_of_segregated_verticies'] = init
             
             # Plot graph 
-            for label in ['difference(final-init)', 'final']:
+            for label in ['difference(final-init)', 'final', 'init']:
                 count = 0
                 min_y = 0
                 max_y = 1
@@ -80,10 +82,11 @@ for world in WORLDS:
                                 x = [count] * len(y)
                                 count += 1
                                 if i == 0:
-                                    plt.scatter(x, y, c=colors[j], alpha=1-k*0.2, 
+                                    plt.scatter(x, y, c=colors[j], alpha=1-k*alpha_rate, s=10,
                                                 label=legend_label_map['%s_%d' % (metric, k)])
                                 else:
-                                    plt.scatter(x, y, c=colors[j], alpha=1-k*0.2)
+                                    plt.scatter(x, y, c=colors[j], alpha=1-k*alpha_rate, s=10)
+                                plt.scatter([x[0]], [np.mean(y)], color=colors[j], alpha=1-k*alpha_rate, s=60)
                                 min_y = min(np.min(y), min_y)
                                 max_y = max(np.max(y), max_y)
                         else:
@@ -91,17 +94,19 @@ for world in WORLDS:
                             x = [count] * len(y)
                             count += 1
                             if i == 0:
-                                plt.scatter(x, y, c=colors[j], label=legend_label_map[metric])
+                                plt.scatter(x, y, c=colors[j], label=legend_label_map[metric], s=10)
                             else:
-                                plt.scatter(x, y, c=colors[j])
+                                plt.scatter(x, y, c=colors[j], s=10)
+                            plt.scatter([x[0]], [np.mean(y)], c=colors[j], s=60)
                             min_y = min(np.min(y), min_y)
                             max_y = max(np.max(y), max_y)
                     plt.vlines(count, min_y, max_y, color='black')
                     count += 1
                 plt.legend()
                 block = count//len(results[label])
-                plt.title('%s %s %s\n%s, mean steps: %.2f' % (world, initialization, swap_cond, label, 
-                                                              np.mean(results['steps'][utility])))
+                mean_steps = [np.mean(results['steps'][u]) for u in results[label].keys()]
+                mean_steps = ', '.join(list(map(lambda x:'%.2f'%(x), mean_steps)))
+                plt.title('%s %s %s\n%s, mean steps: %s' % (world, initialization, swap_cond, label, mean_steps))
                 plt.xticks(range(block//2-1, count, block), results[label].keys())
-                plt.savefig('%s/%s_%s_%s_%s.png' % (dir_path, world, initialization, swap_cond, label))
+                plt.savefig('%s/plots/%s_%s_%s_%s.png' % (dir_path, world, initialization, swap_cond, label))
                 plt.close()
